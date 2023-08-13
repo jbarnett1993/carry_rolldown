ea7980b85c18d98041a0b78be2a425ae36d5ad86
jbarnett1993 <jamesbarnett1993@icloud.com>
2023-08-13 17:08:29 +0100

    changes made to fast and wrong

--- fast_but_wrong_order.py
+++ fast_but_wrong_order.py
@@ -11,14 +11,14 @@ mgr = dm.BbgDataManager()
 base_spot_tickers = {
     'aud': 'ADSW', 'cad': 'CDSW', 'chf': 'SFSNT',
     'eur': 'EUSA', 'gbp': 'BPSWS', 'jpy': 'JYSO',
-    'nzd': 'NDSW', 'sek': 'SKSW', 'usd': 'USOSFR'
+    'nzd': 'NDSWAP', 'sek': 'SKSW', 'usd': 'USOSFR'
 }
 
 # Create an empty dictionary to hold the extended tickers
 spot_curves = {}
 
 # Specify the desired tenors
-spot_tenors = list(range(1, 11)) + [15, 20]
+spot_tenors = list(range(1, 11)) + [15, 20, 25, 30]
 
 # Extend the base tickers for each currency to include specified tenors
 for currency, base_ticker in base_spot_tickers.items():
@@ -26,6 +26,7 @@ for currency, base_ticker in base_spot_tickers.items():
 
 # Convert the spot_curves dictionary to a pandas DataFrame
 spot_curves = pd.DataFrame.from_dict(spot_curves)
+
 
 def get_last_prices(tickers):
     return mgr[tickers].PX_LAST
@@ -38,16 +39,17 @@ last_prices = []
 for i in range(0, n, batch_size):
     tickers_batch = spot_curves.iloc[:, i:i+batch_size].stack().tolist()
     prices = get_last_prices(tickers_batch)
+    prices = prices.reindex(tickers_batch)
     last_prices.extend(prices.values.tolist())
-
-print(tickers_batch)
-print(last_prices)
 
 # Shape the last_prices list to match the spot_curves DataFrame
 last_prices = np.array(last_prices).reshape(n, -1)
 
 
 # Create the updated_spot_curves DataFrame with last prices
-# updated_spot_curves = pd.DataFrame(last_prices, columns=spot_curves.columns)
+updated_spot_curves = pd.DataFrame(last_prices, columns=spot_curves.columns)
 
-# print(updated_spot_curves)
+updated_spot_curves['tenor'] = [f"{i}y" for i in spot_tenors]
+updated_spot_curves.set_index('tenor', inplace=True)
+
+print(updated_spot_curves)
